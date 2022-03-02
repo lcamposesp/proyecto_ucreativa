2dd1c43f29b137e4510fd9e503795d8f2d0a20b1
lcamposesp <99227994+lcamposesp@users.noreply.github.com>
2022-03-01 18:59:39 -0600

    changed test

--- tests/test_practice.py
+++ tests/test_practice.py
@@ -1,5 +1,14 @@
 first_list = ['luis','devops','ucreativa']
 second_list = ['devops',1,2]
 
-def test_sublists():
-    assert set(first_list()).issubset(get(second_list()))
+name = 'Luis'
+last = 'Campos'
+
+def return_name(x):
+    return x
+
+def return_last(y):
+    return y
+
+def test_name(x):
+   assert return_name('Luis') =! return_last('Campos')
